37c9b8a0c0f0401b3577d6f8e9dbd1c2fa73cc3c
greatscotts <javelonagian@live.com>
2013-03-24 17:02:05 +0800

    aus

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,10 +21,10 @@ module ApplicationHelper
     "http://gravatar.com/avatar/#{gravatar_id}.png?s=500&d=#{CGI.escape(default_url)}"  
   end  
  
-  def avatarzz_url(user, options = { :size=> 50 })  
+  def avatarzz_url(user, options = { :size=> 250 })  
     default_url = "http://icons.iconarchive.com/icons/custom-icon-design/pretty-office-4/48/man-icon.png"  
     gravatar_id = Digest::MD5::hexdigest(user.email).downcase  
-    "http://gravatar.com/avatar/#{gravatar_id}.png?s=50&d=#{CGI.escape(default_url)}"  
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=250&d=#{CGI.escape(default_url)}"  
   end  
   
  
